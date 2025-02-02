2664fe5d0c2a97e542f878b1623543908e341876
ayenpam <ayenpampliega22@gmail.com>
2025-02-02 21:52:26 +0800

    Add files via upload

---
+++ PersonalDetails.java
@@ -0,0 +1,14 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        String name = "Ayen Celestine Pampliega";
+        int age = 18;
+        double height = 1.49;
+        boolean isStudent = true;
+ 
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+        System.out.println("Is a Student? " + isStudent);
+    }
+}
+ 
